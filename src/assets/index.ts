3a275df7eac96ecccd813e588303c19679043370
Abay-Angoe <150089877+Abay-Angoe@users.noreply.github.com>
2023-11-07 00:04:38 +0000

    Add files via upload

---
+++ src/assets/index.ts
@@ -0,0 +1,140 @@
+import emailIcon from './Plagiarism-email icon.svg';
+import hideIcon from './Plagiarism-password icon hide.svg';
+import unhiddenIcon from './Plagiarism-password icon unhidden.svg';
+import lockIcon from './Plagiarism-lock.svg';
+import tableMore from './more.svg';
+import arrowLeft from './Plagiarism-arrow left.svg';
+import arrowDown from './arrow-down.svg';
+import logoutIcon from './Plagiarism-logout.svg';
+import logoutIconRed from './logout-red.svg';
+import logoutWhite from './Plagiarism-logout-white.svg';
+import closeIcon from './Plagiarism-close.svg';
+import lecturerIcon from './Frame 55.svg';
+import numStudents from './number-of-students.svg';
+import numStudentSubmissions from './num-student-submissions.svg';
+import totalChecks from './total-checks.svg';
+import passedChecks from './passed-checks.svg';
+import failedChecks from './failed-checks.svg';
+import logo from './plagiarism-logo.svg';
+import trashIcon from './trash.svg';
+import searchIcon from './search-normal.svg';
+import searchWhite from './search-normal white.svg';
+import searchBlue from './search-normal blue.svg';
+import copyIcon from './copy.svg';
+import linkIcon from './link.svg';
+import editIcon from './edit-2.svg';
+
+import lockIconBlue from './lock-blue.svg';
+
+import dashboardIcon from './Plagiarism-dashboard icon.svg';
+import checkIcon from './checker.svg';
+import scheduleIcon from './schedule.svg';
+import unitIcon from './UnitBlue.svg';
+import databaseIcon from './database.svg';
+import manageUsersIcon from './Plagiarism-manage lectures icon.svg';
+import historyIcon from './History.svg';
+import settingIcon from './Plagiarism-setting.svg';
+
+import dashboardIconWhite from './dashboard-white.svg';
+import scheduleWhite from './schedule-white.svg';
+import unitWhite from './book.svg';
+import databaseWhite from './database-white.svg';
+import manageUsersWhite from './manageusers-white.svg';
+import historyWhite from './History-white.svg';
+import settingWhite from './setting-2.svg';
+import checkWhite from './checker-white.svg';
+
+import dashboardIconBlue from './Plagiarism-blue dashboard icon.svg';
+import scheduleBlue from './Plagiarism-blue schedules icon.svg';
+import unitBlue from './Plagiarism-blue academic unit icon.svg';
+import databaseBlue from './Plagiarism-blue database icon.svg';
+import manageUsersBlue from './manageUsers-blue.svg';
+import historyBlue from './Plagiarism-blue history icon.svg';
+import settingBlue from './Plagiarism-blue settings icon.svg';
+import checkBlue from './checker-blue.svg';
+
+import arrowBtnRight from './/table arrow.svg';
+import arrowBtnLeft from './arrowBtn.svg';
+import shareIcon from './share.svg';
+import exportIcon from './export.svg';
+import exportBlack from './exportblack.svg';
+import downloadIcon from './download.svg';
+
+import placeholderImage from './Plagiarism-user.svg';
+import placeholderImageBlue from './user-blue.svg';
+
+import pdfIcon from './Plagiarism-pdf logo.svg';
+import wordIcon from './Plagiarism-Word logo.svg';
+import txtIcon from './Plagiarism-Text logo.svg';
+
+import detailedReport from './detailed-report.svg';
+
+import loadingIcon from './loading.gif';
+import noDataIcon from './icons8-no-data-availible-96.png';
+
+export {
+  lecturerIcon,
+  numStudents,
+  numStudentSubmissions,
+  totalChecks,
+  passedChecks,
+  failedChecks,
+  logo,
+  dashboardIcon,
+  checkIcon,
+  scheduleIcon,
+  unitIcon,
+  editIcon,
+  databaseIcon,
+  manageUsersIcon,
+  historyIcon,
+  settingIcon,
+  emailIcon,
+  hideIcon,
+  unhiddenIcon,
+  lockIcon,
+  lockIconBlue,
+  arrowLeft,
+  logoutIcon,
+  logoutIconRed,
+  logoutWhite,
+  closeIcon,
+  arrowBtnLeft,
+  arrowBtnRight,
+  shareIcon,
+  exportIcon,
+  downloadIcon,
+  placeholderImage,
+  placeholderImageBlue,
+  settingWhite,
+  dashboardIconWhite,
+  databaseWhite,
+  unitWhite,
+  manageUsersWhite,
+  scheduleWhite,
+  historyWhite,
+  checkWhite,
+  settingBlue,
+  historyBlue,
+  manageUsersBlue,
+  databaseBlue,
+  unitBlue,
+  scheduleBlue,
+  dashboardIconBlue,
+  checkBlue,
+  txtIcon,
+  pdfIcon,
+  wordIcon,
+  arrowDown,
+  detailedReport,
+  tableMore,
+  loadingIcon,
+  trashIcon,
+  exportBlack,
+  linkIcon,
+  copyIcon,
+  searchIcon,
+  searchWhite,
+  searchBlue,
+  noDataIcon,
+};
